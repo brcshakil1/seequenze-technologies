d557a2e2c2337fb460912838b98416f4f201feb7
brcshakil1 <brcshakil72@gmail.com>
2023-12-30 19:41:16 +0600

    added spinner from dasisy ui white loading data

--- client/src/pages/Dashboard/SampleProjects/SampleProjects.jsx
+++ client/src/pages/Dashboard/SampleProjects/SampleProjects.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import ProjectCard from "../../../components/shared/ProjectCard";
 import SectionTitle from "./../../../components/ui/SectionTitle";
+import Loading from "../../../components/ui/Loading";
 
 const SampleProjects = () => {
   const { data: sampleProjects, isPending } = useQuery({
@@ -14,18 +15,22 @@ const SampleProjects = () => {
     },
   });
 
-  console.log(sampleProjects);
-
   return (
-    <div className="mx-4 pl-0 md:pl-10 lg:pl-[52px]">
-      <div>
-        <SectionTitle title="Sample Projects" />
-      </div>
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 py-7">
-        {sampleProjects?.map((project) => (
-          <ProjectCard key={project?.id} project={project} />
-        ))}
-      </div>
+    <div>
+      {isPending ? (
+        <Loading />
+      ) : (
+        <div className="mx-4 pl-0 md:pl-10 lg:pl-[52px]">
+          <div>
+            <SectionTitle title="Sample Projects" />
+          </div>
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 py-7">
+            {sampleProjects?.map((project) => (
+              <ProjectCard key={project?.id} project={project} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
